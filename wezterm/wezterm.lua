61732cd13b5ac89b7ba379c7f0b0d67058269361
Mark-MacBook <markmaiw_0007@hotmail.com>
2025-03-05 01:27:56 +0700

    Change font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("Lotion")
-config.font_size = 14
+config.font = wezterm.font("MesloLGS NF")
+-- config.font = wezterm.font("Lotion")
+config.font_size = 12
 
 return config
